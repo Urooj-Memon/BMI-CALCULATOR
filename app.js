59ed82dfb21a8053892861bdc1154813c39d95ed
Urooj-Memon <danishmemon2016@gmail.com>
2024-04-20 00:48:40 +0500

    BMI-Calculator

---
+++ app.js
@@ -0,0 +1,4 @@
+var weightInkg = 68;
+var heightInmeters = 1.75;
+var bmi = weightInkg / (heightInmeters * heightInmeters);
+console.log("Your BMI ".concat(bmi));
---
+++ app.ts
@@ -0,0 +1,7 @@
+let weightInkg = 68;
+
+let heightInmeters = 1.75;
+
+let bmi = weightInkg / (heightInmeters * heightInmeters);
+
+console.log (`Your BMI ${bmi}`);
